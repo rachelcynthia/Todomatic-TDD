aae759d6c17995294e33910a6b791b59a57ec0cf
Rachel Cynthia <rachelwork@Rachels-MacBook-Pro.local>
2022-02-08 20:20:56 +0530

    Add. test to check label and delete button

--- src/components/Todo/Todo.test.js
+++ src/components/Todo/Todo.test.js
@@ -9,7 +9,8 @@ Enzyme.configure({ adapter: new Adapter() });
 let completedProps = {
     id:1, 
     title:"todo", 
-    completed : true
+    completed : true,
+    deleteTask :jest.fn()
 }
 
 let notCompletedProps = {
@@ -32,14 +33,25 @@ describe("Basic Rendering of Todo", () =>{
         const { getByTestId }= render(<Todo />)
         expect(getByTestId("checkbox")).not.toHaveAttribute("disabled")
     })
-    
-    it("Checkbox must be checked when it is completed", () => {
+
+    it("Should have checkbox which must be checked when it is completed", () => {
         const todo = render(<Todo {...completedProps}/>);
         expect(todo.getByTestId("checkbox")).toBeChecked()
     })
 
-    it("Checkbox must not be checked when not completed", () => {
+    it("Should have checkbox which must not be checked when not completed", () => {
         const todo = render(<Todo {...notCompletedProps}/>);
         expect(todo.getByTestId("checkbox")).not.toBeChecked()
+    })
+
+    it("Should have label which must contain the correct label", () =>{
+        const todo = render(<Todo {...completedProps}/>);
+        expect(todo.getByTestId("todo-label").textContent).toBe(" todo ")
+    })
+
+    it("Should call delete task when button is clicked", ()=> {
+        const todo = render(<Todo {...completedProps}/>);
+        fireEvent.click(todo.getByTestId("delete-todo"))
+        expect(completedProps.deleteTask).toHaveBeenCalledTimes(1)
     })
 })
